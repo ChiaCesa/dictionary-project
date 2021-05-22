2d74ea57966cf4777d85aa00b9f454c3c5cec446
Chiara <kia.cesa@gmail.com>
2021-05-22 15:12:28 +0200

    search engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import catBanner from './catBanner.jpg';
 import './App.css';
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
        <h1> Welcome to Dictionary app!</h1>
         <img src={catBanner} className="App-logo" alt="logo" />
-        
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">Coded by Chiara Cesana</footer>
+      </div>
     </div>
   );
 }
